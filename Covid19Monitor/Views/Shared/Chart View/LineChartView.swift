de704e230572d8ed8c2211a8b05f46272ad11959
Luntu <luntu.mavukz35@gmail.com>
2020-08-13 08:56:09 +0200

    Added chart framework

---
+++ Covid19Monitor/Views/Shared/Chart View/LineChartView.swift
@@ -0,0 +1,13 @@
+//
+//  LineChartView.swift
+//  Covid19Monitor
+//
+//  Created by Luntu on 2020/07/25.
+//  Copyright © 2020 Luntu. All rights reserved.
+//
+
+import UIKit
+
+class LineChartView: UIView {
+    
+}
